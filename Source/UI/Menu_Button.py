7a0284582b3e93c0477ff848fe5e7b8a6cb62820
esg <jelle.bekkers@live.nl>
2026-07-28 13:19:13 +0200

    Moved menu button
    +Moved menu button to a better location

--- Source/UI/Menu_Button.py
+++ Source/UI/Menu_Button.py
@@ -4,8 +4,8 @@ import os
 from Source.Window_Manager import canvas
 from Source.Config.paths import UI_DIR
 
-MENU_OFFSET_X = -60
-MENU_OFFSET_Y = -30
+MENU_OFFSET_X = 60
+MENU_OFFSET_Y = 50
 
 menu_image = None
 menu_hover = None
